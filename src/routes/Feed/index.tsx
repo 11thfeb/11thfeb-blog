9cd0dd3e8fd5bfa577dc5190e57fa9d39a5d924a
11th.feb <anhbi11o2@gmail.com>
2024-03-22 10:54:34 +0700

    add analytic for page

--- src/routes/Feed/index.tsx
+++ src/routes/Feed/index.tsx
@@ -10,6 +10,7 @@ import ProfileCard from "./ProfileCard"
 import ServiceCard from "./ServiceCard"
 import ContactCard from "./ContactCard"
 import PostList from "./PostList"
+import Analytic from "./Analytic"
 
 const HEADER_HEIGHT = 73
 
@@ -49,6 +50,7 @@ const Feed: React.FC<Props> = () => {
         <ProfileCard />
         <ServiceCard />
         <ContactCard />
+        <Analytic />
         <div className="footer">
           <Footer />
         </div>
