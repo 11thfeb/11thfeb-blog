09b06db6c298e8802f73ab3dec957dd72e6696c0
11thfeb <anhbi11o2@gmail.com>
2025-04-30 19:54:10 +0700

    fix post.status

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 module.exports = {
+  staticPageGenerationTimeout: 180,
   images: {
     domains: [
-      'www.notion.so',
-      'lh3.googleusercontent.com',
-      'lh5.googleusercontent.com',
-      's3-us-west-2.amazonaws.com']
-  }
+      "www.notion.so",
+      "lh3.googleusercontent.com",
+      "lh5.googleusercontent.com",
+      "s3-us-west-2.amazonaws.com",
+    ],
+  },
+  experimental: {
+    workerThreads: false,
+    cpus: 1,
+  },
 }
